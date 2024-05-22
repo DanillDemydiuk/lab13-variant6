230e0be6ca18eb33eb446759868fed596a36d418
Vergil <Danill.Demydiuk@infiz.khpi.edu.ua>
2024-05-22 20:01:16 +0200

    Добавьте файлы проекта.

---
+++ lab13/lab13.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+// Рекурсивна функція для обчислення n-го члена ряду
+int ряд(int n) {
+    if (n == 1) {
+        return 1; 
+    }
+    return ряд(n - 1) + 2; 
+}
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    int n;
+    cout << "Введіть номер члена ряду: ";
+    cin >> n;
+    cout << n << "-й член ряду: " << ряд(n) << endl;
+    return 0;
+}
